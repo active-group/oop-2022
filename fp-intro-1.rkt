010ef1ea9edfdd6d4866a3368329a4d0adf1e8b3
Mike Sperber <sperber@deinprogramm.de>
2022-01-31 10:26:34 +0100

    Update.

--- fp-intro-1.rkt
+++ fp-intro-1.rkt
@@ -12,3 +12,7 @@
 (define star1 (star 50 "solid" "green"))
 (define square1 (square 100 "outline" "blue"))
 (define overlay1 (overlay star1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
