e8fce988821de1eae9833ddba4a62e9a3c9bfe5d
Mike Sperber <sperber@deinprogramm.de>
2022-01-31 10:14:35 +0100

    Update.

--- fp-intro-1.rkt
+++ fp-intro-1.rkt
@@ -1,4 +1,5 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname fp-intro-1) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
-
+; If you don't see image.rkt:
+; Go to Language -> Add Teachpack -> image.rkt -> OK
